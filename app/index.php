e7e024d2786cad1f78f1fb2e0802a23b8b2813de
mickel075 <devriesmickel@gmail.com>
2026-01-07 11:30:10 +0100

    Update index.php

--- app/index.php
+++ app/index.php
@@ -1,16 +1,51 @@
+<!-- Registratiepagina:
+
+Maak een HTML-formulier voor registratie met invoervelden voor gebruikersnaam en wachtwoord.
+Verstuur de ingevulde gegevens naar een PHP-script voor verwerking. -->
+
 <?php
 
-$host = "mysql"; // Le host est le nom du service, présent dans le docker-compose.yml
-$dbname = "my-wonderful-website";
-$charset = "utf8";
-$port = "3306";
+$host = "mysql"; // de hostnaam van de database server
+$user = "root"; // de gebruikersnaam voor de database
+$pass = "rootpassword"; // het wachtwoord voor de database
+$dbname = "users_login"; // de naam van de database
+$charset = "utf8"; // de tekencodering
+$port = "3306"; // de poortnummer
+
 ?>
 
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <title>Drenthe College docker web server</title>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrationpage</title>
+    <link rel="stylesheet" href="styles.css">
+
 </head>
 <body>
-<h2>Hello world!</h2>
+
+    <!-- div container -->
+    <div class="container"> 
+
+        <h2>Register</h2>
+
+        <!-- formulier voor het registreren van de user -->
+        <form action="register.php" method="POST">
+
+            <label for="username">username:</label>
+            <input type="text" id="username" name="username" required>
+
+            <label for="password">password:</label>
+            <input type="password" id="password" name="password" required>
+
+            <button type="submit">Register</button>
+
+        </form>
+
+    </div>
+
 </body>
 </html>
